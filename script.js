ce0fda0377afde392d83d904c979f2e2ec77e5e2
ShamanOfTheAleClan <paulius.sviackevicius@gmail.com>
2019-06-22 23:55:57 +0300

    added API, but not yet functional

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ for (i=1; i < rows.length; i++) {
         rows[i].classList.add("discount")
     }
 }
+
+
+//XLM
+
+
+
+let url = 'https://api.unsplash.com/photos/?count=20&client_id=0e489bafc809bbb304de7230134f1a22096c706a20c0fa9537ddb7cb982c4543';
+
+let data;
+let imgUrlArray = [];
+let http = new XMLHttpRequest();
+
+window.onload=function() {
+
+	http.onreadystatechange = function() {
+		console.log(http);
+		if (http.readyState == 4 && http.status == 200) {
+            data = JSON.parse(http.response);
+            for (i=0; i < data.length; i++) {
+                let imgUrl = data[i].urls.small;
+                imgUrlArray.push(imgUrl);
+            }
+		}
+	};
+    
+	http.open("GET", url);
+    http.send();
+    
+    let images = document.querySelectorAll('.news-item');
+    for (i=0; i < images.length; i++) {
+        images[i]
+    }
+};
