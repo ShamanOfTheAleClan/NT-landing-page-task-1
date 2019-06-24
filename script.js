3a64ed9c634099099792d03dccaf0d66853ba5e3
ShamanOfTheAleClan <paulius.sviackevicius@gmail.com>
2019-06-24 17:59:07 +0300

    Cleaned up the code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Apply WOW on JS generated items
 let selectItems = document.querySelectorAll('.select-selected');
-
 for (let i =0; i < selectItems.length; i++) {
     selectItems[i].classList.add('wow');
     selectItems[i].classList.add('fadeInUp');
